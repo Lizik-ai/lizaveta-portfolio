caebf9480ce26a17d23b44fbd69604b8e2261ca6
Lizaveta Lamaka <smurfus52@gmail.com>
2026-07-29 15:42:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    # Render сам выдает порт в переменные окружения, если его нет - берем стандартный 5000
+    port = int(os.environ.get("PORT", 5000))
+    # Обязательно слушаем 0.0.0.0, чтобы сервер открыл порты наружу
+    app.run(host='0.0.0.0', port=port)
